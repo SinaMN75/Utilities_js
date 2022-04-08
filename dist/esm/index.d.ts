112e6d8837e4f7213268f2cbb04b9f5ef814bc71
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-04-08 15:24:16 +0430

    :)

--- dist/esm/index.d.ts
+++ dist/esm/index.d.ts
@@ -2,8 +2,3 @@ export * from "./components/components";
 export * from "./utils/utils";
 export * from "./data/models/models";
 export * from "./core/core";
-export * from "axios";
-export * from "react-dom";
-export * from "prop-types";
-import React from "react";
-export { React };
--- dist/esm/index.js
+++ dist/esm/index.js
@@ -2,9 +2,4 @@ export * from "./components/components";
 export * from "./utils/utils";
 export * from "./data/models/models";
 export * from "./core/core";
-export * from "axios";
-export * from "react-dom";
-export * from "prop-types";
-import React from "react";
-export { React };
 //# sourceMappingURL=index.js.map
