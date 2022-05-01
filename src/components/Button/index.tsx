0f5a12977fd83567d99c48694ec32bccdfd07c51
MohamadReza Heydari <heydari.more@gmail.com>
2022-05-01 12:13:22 +0430

    Add <add component Button>

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,27 @@
+import React , { HTMLAttributes, memo, ReactElement } from "react";
+
+interface Props extends HTMLAttributes<HTMLDivElement>{
+   borderColor: "border-color-orange" | "border-color-green";
+   textButton: string;
+   notification: boolean;
+}
+
+function Button({ children, borderColor, textButton, notification }: Props): ReactElement {
+    return (  
+        <div className="button-component">
+            {notification === true ? (
+                <span className="notification">3</span>
+            ) : (null)}
+            <button className={`button ${borderColor}`}>
+                {children}
+                {textButton !== '' ? (
+                    <h3 className="text-button">
+                        {textButton}
+                    </h3>
+                ) : (null)}
+            </button>
+        </div>
+    );
+}
+
+export default memo(Button);
