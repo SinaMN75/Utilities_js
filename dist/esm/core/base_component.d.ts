27e57cab51088e6523a05b3f49755dbe45ff3c8e
MohamadReza Heydari <heydari.more@gmail.com>
2022-05-01 12:29:38 +0430

    Add <new build>'

--- dist/esm/core/base_component.d.ts
+++
@@ -1,6 +0,0 @@
-import { Component, PureComponent } from 'react';
-export declare class BaseComponent<P, S> extends Component<P, S> {
-    showLoading(): void;
-}
-export declare class BasePureComponent<P, S> extends PureComponent<P, S> {
-}
--- dist/esm/utils/utils.js
+++
@@ -1,3 +0,0 @@
-export * from "./http_interceptor";
-export * from "./local_storage";
-//# sourceMappingURL=utils.js.map
