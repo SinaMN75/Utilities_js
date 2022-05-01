b9cabcf8de939f18545f372a9df3a3bb2ff459fc
MohamadReza Heydari <heydari.more@gmail.com>
2022-05-01 15:19:49 +0430

    Add <new build>

--- dist/cjs/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./components/components";
-export * from "./utils/utils";
-export * from "./data/models/models";
-export * from "./core/core";
-import './styles/app.css';
--- dist/esm/components/Button/index.js
+++ dist/esm/components/Button/index.js
@@ -1,6 +1,6 @@
 import React, { memo } from "react";
 function Button({ children, borderColor, textButton, notification }) {
-    return (React.createElement("div", { className: "button-component" },
+    return (React.createElement("div", { className: "button-component", style: { backgroundColor: "red" } },
         notification === true ? (React.createElement("span", { className: "notification" }, "3")) : (null),
         React.createElement("button", { className: `button ${borderColor}` },
             children,
