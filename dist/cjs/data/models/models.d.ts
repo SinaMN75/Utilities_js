ff82c8b1bc084abf273fb4a9fdc844d6bd104163
MohamadReza Heydari <heydari.more@gmail.com>
2022-05-01 12:33:00 +0430

    Add <new build>'

--- dist/cjs/data/models/models.d.ts
+++
@@ -1,1 +0,0 @@
-export * from "./responses/pokemon_response";
--- dist/esm/utils/http_interceptor.d.ts
+++
@@ -1,6 +0,0 @@
-import { Method } from "axios";
-export declare function request(method: Method, url: string, body: object | null, onResponse: (response: any) => any, onError: (error: any) => any): void;
-export declare function httpGet(url: string, onResponse: (response: any) => any, onError: (response: any) => any): void;
-export declare function httpPost(url: string, body: object | null | undefined, onResponse: (response: any) => any, onError: (response: any) => any): void;
-export declare function httpPut(url: string, body: object | null | undefined, onResponse: (response: any) => any, onError: (response: any) => any): void;
-export declare function httpDelete(url: string, body: object | null | undefined, onResponse: (response: any) => any, onError: (response: any) => any): void;
