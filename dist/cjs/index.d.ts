bb9b152ed9eb4516438d5ff63b9b64b03ca0c746
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-05-06 09:57:40 +0430

    code clean

--- dist/cjs/index.d.ts
+++ dist/cjs/index.d.ts
@@ -1,10 +1,4 @@
-import React from "react";
-import PropTypes from "prop-types";
 export * from "./components/components";
 export * from "./utils/utils";
 export * from "./data/models/models";
 export * from "./core/core";
-export * from "axios";
-export * from "prop-types";
-export * from "react-dom";
-export { React, PropTypes };
--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -13,20 +13,9 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
 var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.PropTypes = exports.React = void 0;
-const react_1 = __importDefault(require("react"));
-exports.React = react_1.default;
-const prop_types_1 = __importDefault(require("prop-types"));
-exports.PropTypes = prop_types_1.default;
 __exportStar(require("./components/components"), exports);
 __exportStar(require("./utils/utils"), exports);
 __exportStar(require("./data/models/models"), exports);
 __exportStar(require("./core/core"), exports);
-__exportStar(require("axios"), exports);
-__exportStar(require("prop-types"), exports);
-__exportStar(require("react-dom"), exports);
 //# sourceMappingURL=index.js.map
