6873533c67b47955e7e43e31df119cac02586c37
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-05-06 09:49:41 +0430

    code clean

--- dist/esm/index.js
+++ dist/esm/index.js
@@ -1,5 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
 export * from "./components/components";
 export * from "./utils/utils";
 export * from "./data/models/models";
 export * from "./core/core";
+export * from "axios";
+export * from "prop-types";
+export * from "react-dom";
+export { React, PropTypes };
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
 export * from "./components/components";
 export * from "./utils/utils";
 export * from "./data/models/models";
 export * from "./core/core";
+export * from "axios";
+export * from "prop-types";
+export * from "react-dom";
+
+export {React, PropTypes}
