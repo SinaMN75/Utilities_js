e1e90b202a12d42dbcb8af2236801e5310544f3c
eliham <elham.babolhavaeji74@gmail.com>
2022-05-29 02:32:28 +0430

    fixed bug

--- dist/esm/data/form_datasource.d.ts
+++ dist/esm/data/form_datasource.d.ts
@@ -4,6 +4,6 @@ export declare class FormDataSource {
     baseUrl: string;
     constructor(baseUrl: string);
     createFormField(dto: FormFieldDto, categoryId: string, onResponse: (response: GenericResponse<FormFieldDto>) => any, onError: (response: Response) => any): Promise<void>;
-    readFormFields(categoryId: string, onResponse: (response: GenericResponse<FormFieldDto>) => any, onError: (response: Response) => any): Promise<void>;
+    readFormFields(categoryId: string, onResponse: (response: GenericResponse<FormFieldDto[]>) => any, onError: (response: Response) => any): Promise<void>;
     createForm(dto: FormCreateDto, onResponse: (response: GenericResponse<FormFieldDto>) => any, onError: (response: Response) => any): Promise<void>;
 }
