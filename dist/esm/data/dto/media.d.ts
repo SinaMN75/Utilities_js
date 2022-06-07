ebadb7ebc24fdb57de9ee562ae0e083ab941155f
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-06-07 19:14:20 +0430

    add usecase

--- dist/esm/data/dto/media.d.ts
+++ dist/esm/data/dto/media.d.ts
@@ -16,6 +16,7 @@ export interface MediaCreateUpdateDto {
     TenderId?: string;
     ServiceId?: string;
     MagazineId?: string;
+    UseCase?: string;
     Files?: File[];
     Links?: string[];
 }
