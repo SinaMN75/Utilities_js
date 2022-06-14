034bffe722493f0f7b5508626512fa0ce621d5f7
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-06-14 22:27:01 +0430

    fix

--- dist/esm/components/components.js
+++
@@ -1,4 +0,0 @@
-export { Button } from "./button";
-export { IconTextVertical } from "./Icon_text_vertical";
-export { TabNavigation } from "./tab_navigation";
-//# sourceMappingURL=components.js.map
