b52c37a50e73abb4672aeb7cb393bfd96fd36633
eliham <elham.babolhavaeji74@gmail.com>
2022-07-16 12:06:07 +0430

    added report read dto

--- dist/esm/data/dto/report.d.ts
+++ dist/esm/data/dto/report.d.ts
@@ -1,6 +1,58 @@
+import { CategoryReadDto } from "./category";
+import { FormFieldDto } from "./form";
+import { MediaReadDto } from "./media";
 export interface ReportCreateUpdateDto {
     id?: string;
     title?: string;
     description?: string;
     productId?: string;
+}
+export interface ReportFilterDto {
+    user?: boolean;
+    product?: boolean;
+}
+export interface ReportReadDto {
+    id?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+    deletedAt?: Date;
+    title?: string;
+    subtitle?: string;
+    description?: string;
+    details?: string;
+    address?: string;
+    author?: string;
+    phoneNumber?: string;
+    link?: string;
+    website?: string;
+    email?: string;
+    type?: string;
+    useCase?: string;
+    unit?: string;
+    latitude?: number;
+    longitude?: number;
+    length?: number;
+    width?: number;
+    height?: number;
+    weight?: number;
+    minOrder?: number;
+    maxOrder?: number;
+    price?: number;
+    isForSale?: boolean;
+    enabled?: boolean;
+    visitsCount?: number;
+    startDate?: Date;
+    endDate?: Date;
+    status?: number;
+    userId?: string;
+    user?: string;
+    locations?: Location[];
+    media?: MediaReadDto[];
+    forms?: FormFieldDto[];
+    categories?: CategoryReadDto[];
+    voteFields?: CategoryReadDto[];
+    reports?: any;
+    bookmarks?: any;
+    comments?: Comment[];
+    teams?: CategoryReadDto[];
 }
