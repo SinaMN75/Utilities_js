88499bdf7aa3146bce708598da807777de9d7d44
eliham <elham.babolhavaeji74@gmail.com>
2022-08-23 13:44:36 +0430

    added ProductV2FilterDto

--- dist/cjs/data/productV2_datasource.d.ts
+++ dist/cjs/data/productV2_datasource.d.ts
@@ -1,10 +1,10 @@
-import { GenericResponse, ProductCreateUpdateDto, ProductFilterDto, ProductReadDto } from "./data";
+import { GenericResponse, ProductCreateUpdateDto, ProductV2FilterDto, ProductReadDto } from "./data";
 export declare class ProductV2DataSource {
     baseUrl: string;
     constructor(baseUrl: string);
     create(dto: ProductCreateUpdateDto, onResponse: (response: GenericResponse<ProductReadDto>) => any, onError: (response: Response) => any): Promise<void>;
     update(dto: ProductCreateUpdateDto, onResponse: (response: GenericResponse<ProductReadDto>) => any, onError: (response: Response) => any): Promise<void>;
-    filter(dto: ProductFilterDto, onResponse: (response: GenericResponse<ProductReadDto[]>) => any, onError: (response: Response) => any): Promise<void>;
+    filter(dto: ProductV2FilterDto, onResponse: (response: GenericResponse<ProductReadDto[]>) => any, onError: (response: Response) => any): Promise<void>;
     readById(id: string, onResponse: (response: GenericResponse<ProductReadDto>) => any, onError: (response: Response) => any): Promise<void>;
     delete(id: string, onResponse: (response: Response) => any, onError: (response: Response) => any): Promise<void>;
 }
