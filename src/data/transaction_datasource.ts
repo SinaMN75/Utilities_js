45ef7a2d32789f23f0c4c90f04caca1a54075b6f
eliham <elham.babolhavaeji74@gmail.com>
2022-08-24 22:55:23 +0430

    added transaction data source and dto

---
+++ src/data/transaction_datasource.ts
@@ -0,0 +1,22 @@
+import {httpDelete, httpGet, httpPost, httpPut} from "../utils/http_interceptor";
+import {GenericResponse, ProductCreateUpdateDto, ProductReadDto} from "./data";
+import {TransactionReadDto} from "./dto/transaction";
+
+export class TransactionDataSource {
+	baseUrl: string;
+
+	constructor(baseUrl: string) {
+		this.baseUrl = baseUrl;
+	}
+
+	
+
+	async read(onResponse: (response: GenericResponse<TransactionReadDto[]>) => any,
+	           onError: (response: Response) => any) {
+		await httpGet(`${this.baseUrl}content`,
+			response => onResponse(response),
+			response => onError(response)
+		);
+	}
+
+}
